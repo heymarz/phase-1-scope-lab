32758765f847fd4138567db65998fbdb4591ea04
Marlena Tsang <Marlena345@gmail.com>
2021-09-23 15:33:29 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+//bob is declared on the global scope
+var customerName = "bob";
+
+//modify to uppercase
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+
+function setBestCustomer () {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Daniel";
+  }
